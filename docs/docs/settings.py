f1710f21114d93fbcc26cee2b23a0b36ebb8323a
Loren Davie <loren@axilent.com>
2012-05-27 14:07:39 -0400

    Settings_local stuff moved into environment variables.
    In preparation for Heroku deployment, the stuff previously in
    settings_local has been moved into environment variables.  Also added
    local.sh for easy startup for local development (not in version
    control) and a local.sh.template file to show how to create a local.sh
    file.

--- docs/docs/settings.py
+++ docs/docs/settings.py
@@ -1,5 +1,27 @@
 # Django settings for docs project.
 
+# =========================
+# = ENVIRONMENT VARIABLES =
+# =========================
+# Former local settings found below - now using environment variables
+# Environment Variables are
+#
+# AXILENT_DOCS_DEBUG (False)
+# AXILENT_DOCS_ADMIN_NAME ('Ops')
+# AXILENT_DOCS_ADMIN_EMAIL ('ops@axilent.com')
+# AXILENT_DOCS_DB_ENGINE ('django.db.backends.sqlite3')
+# AXILENT_DOCS_DB_NAME ('axilentdocs')
+
+# AXILENT_DOCS_PROJECT_ROOT ($PWD)
+# AXILENT_DOCS_STATIC_ROOT ('<project-root>/collected-static/')
+# AXILENT_DOCS_STATICFILES_DIRS ('<project-root>/common-static/')
+# AXILENT_DOCS_STATIC_URL ('/static/')
+# AXILENT_DOCS_SECRET_DJANGO_KEY
+
+# AXILENT_DOCS_API_KEY
+# AXILENT_DOCS_ENDPOINT ('https://www.axilent.net')
+
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -72,5 +94,104 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-from settings_local import *
+import os
 
+DEBUG = True if os.environ.get('AXILENT_DOCS_DEBUG',None) else False
+TEMPLATE_DEBUG = DEBUG
+
+admin_name = os.environ.get('AXILENT_DOCS_ADMIN_NAME','Ops')
+admin_email = os.environ.get('AXILENT_DOCS_ADMIN_EMAIL','ops@axilent.com')
+ADMINS = (
+    (admin_name,admin_email),
+)
+
+MANAGERS = ADMINS
+
+db_engine = os.environ.get('AXILENT_DOCS_DB_ENGINE','django.db.backends.sqlite3')
+db_name = os.environ.get('AXILENT_DOCS_DB_NAME','axilentdocs')
+DATABASES = {
+    'default': {
+        'ENGINE': db_engine, # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': db_name,                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+project_root = os.environ.get('AXILENT_DOCS_PROJECT_ROOT',os.environ['PWD']+'/../') # locally manage.py is invoked one level down from project root
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+static_root = os.environ.get('AXILENT_DOCS_STATIC_ROOT',project_root+'collected-static/')
+STATIC_ROOT = static_root
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+static_url = os.environ.get('AXILENT_DOCS_STATIC_URL','/static/')
+STATIC_URL = static_url
+
+# Additional locations of static files
+staticfiles_dirs = os.environ.get('AXILENT_DOCS_STATICFILES_DIRS',project_root+'common-static/')
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    staticfiles_dirs,
+)
+
+print 'staticfiles dirs are',staticfiles_dirs
+
+# Make this unique, and don't share it with anybody.
+# SECRET_KEY = os.environ['AXILENT_DOCS_SECRET_DJANGO_KEY']
+SECRET_KEY = '2541b&amp;b6(hg(b8zc7%4=6ylx%&amp;w^#b1uf%f5vd7ovv6^h8!0f^' # TODO!!!!
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters':{
+        'simple':{
+            'format':'%(levelname)s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'formatter':'simple',
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        'django-axilent':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        }
+    }
+}
+
+AXILENT_API_KEY = os.environ['AXILENT_DOCS_API_KEY']
+AXILENT_ENDPOINT = os.environ.get('AXILENT_DOCS_ENDPOINT','https://www.axilent.net')
+#AXILENT_API_KEY = '2b9588c57c18414d9b2f529c74d32dc5' # TODO
