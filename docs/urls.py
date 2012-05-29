78eeed8e43df3ecb76e4c3ce3cf9a74f7474d72a
Peter Herndon <tpherndon@gmail.com>
2012-05-29 15:13:30 -0400

    Fixing stupid typography quotes from copy-and-paste error

--- docs/urls.py
+++ docs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,6 +21,6 @@ urlpatterns = patterns('docs.views',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns(”,
-(r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
+urlpatterns += patterns('',
+(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
